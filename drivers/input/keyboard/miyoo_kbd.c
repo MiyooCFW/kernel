b721f9d04cd6c0f5d629e8074985a1c2b5aa5bc9
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-05-11 08:06:06 +0200

    rework screenshot hotkey

--- drivers/input/keyboard/miyoo_kbd.c
+++ drivers/input/keyboard/miyoo_kbd.c
@@ -614,7 +614,7 @@ static void scan_handler(unsigned long unused)
     }
     if((val & MY_R) && (val & MY_TA)) {
       if(!hotkey_down) {
-        static char * screenshot_argv[] = {"/bin/sh", "-c", "/mnt/apps/fbgrab/screenshot.sh", NULL};
+        static char * screenshot_argv[] = {"/bin/sh", "-c", "mkdir - p /mnt/screenshots ; name=/mnt/screenshots/system ; if test -e $name.png ; then i=1 ; while test -e $name-$i.png ; do i=$((i+1)) ; done; name=\"$name-$i\" ; fi ; /usr/bin/fbgrab \"$name\".png", NULL};
         call_usermodehelper(screenshot_argv[0], screenshot_argv, NULL, UMH_NO_WAIT);
         hotkey_down = true;
       }
@@ -817,7 +817,7 @@ static void scan_handler(unsigned long unused)
 		}
 		else if((val & MY_R) && (val & MY_START)){
       if(!hotkey_down) {
-        static char * screenshot_argv[] = {"/bin/sh", "-c", "name=/mnt/system; if test -e $name.png ; then i=1 ; while test -e $name-$i.png ; do i=$((i+1)) ; done; name=\"$name-$i\" ; fi ; /usr/bin/fbgrab \"$name\".png", NULL};
+        static char * screenshot_argv[] = {"/bin/sh", "-c", "mkdir - p /mnt/screenshots ; name=/mnt/screenshots/system ; if test -e $name.png ; then i=1 ; while test -e $name-$i.png ; do i=$((i+1)) ; done; name=\"$name-$i\" ; fi ; /usr/bin/fbgrab \"$name\".png", NULL};
         call_usermodehelper(screenshot_argv[0], screenshot_argv, NULL, UMH_NO_WAIT);
         hotkey_down = true;
       }

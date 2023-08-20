46de701baca2fb347e2921c3abf73d6534e679c4
Apaczer <94932128+Apaczer@users.noreply.github.com>
2023-08-20 21:56:04 +0200

    [R61520] set Inversion OFF when invert=0

--- drivers/video/fbdev/r61520fb.c
+++ drivers/video/fbdev/r61520fb.c
@@ -682,7 +682,9 @@ if(debug){
     gpio_wr_cmd(0x13);
 
     if (invert) {
-        gpio_wr_cmd(0x21); // invert colors
+      gpio_wr_cmd(0x21); // enter_invert_mode for colors
+    } else {
+      gpio_wr_cmd(0x20); // exit_invert_mode for colors
     }
 
     gpio_wr_cmd(0x35);
@@ -783,7 +785,9 @@ if(debug){
     gpio_wr_dat(0x13); // or 0x0b?
 
     if (invert) {
-        gpio_wr_cmd(0x21); // invert colors
+      gpio_wr_cmd(0x21); // Display Inversion On (INVON for colors)
+    } else {
+      gpio_wr_cmd(0x20); //  Display Inversion Off (INVOFF for colors)
     }
 
     gpio_wr_cmd(0xc4);
